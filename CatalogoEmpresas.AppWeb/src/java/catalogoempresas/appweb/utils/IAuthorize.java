4ac67a9ef009a5a273e3104cbea367a40d08f341
Carlos Mangandí <me22001@esfe.agape.edu.sv>
2023-08-21 08:16:29 -0600

    Codificación de los elementos del paquete Utils del proyecto AppWeb

--- CatalogoEmpresas.AppWeb/src/java/catalogoempresas/appweb/utils/IAuthorize.java
+++ CatalogoEmpresas.AppWeb/src/java/catalogoempresas/appweb/utils/IAuthorize.java
@@ -1,7 +1,9 @@
 
 package catalogoempresas.appweb.utils;
 
+import jakarta.servlet.ServletException;
+import java.io.IOException;
 
 public interface IAuthorize {
-    
+    void authorize() throws ServletException, IOException; // La interfaz solo define los metodos pero no los implementa
 }
